2aa48cd8544f4c89e8ab3b1ce3309e9671dcaa83
Gabriel Veloso <gaabrielvelooso@gmail.com>
2024-12-27 14:50:12 -0300

    Rename application class

--- src/main/java/com/github/gabrielvelosoo/productsapi/Application.java
+++ src/main/java/com/github/gabrielvelosoo/productsapi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductsApiApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductsApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
